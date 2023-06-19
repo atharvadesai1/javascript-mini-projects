d94569f3a909ddad3cac8056bc7b57126ec36bd0
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2023-06-19 19:44:56 +0530

    done

--- crud_add_remove.js
+++ crud_add_remove.js
@@ -1,6 +1,9 @@
 let addState = document.getElementById('addBtn');
-        addState.addEventListener('click',addBook);
-        let parentList = document.getElementById('parentList');
+addState.addEventListener('click',addBook);
+let parentList = document.getElementById('parentList');
+let msgIntend = document.getElementById('msgImp');
+let msgImp = document.getElementById('msgImp');
+
         // removeState.addEventListener('click',removeBook);
 
         if(parentList.childElementCount===0){
@@ -13,6 +16,12 @@ let addState = document.getElementById('addBtn');
         }
 
         function addBook(e){
+            if(msgImp.childElementCount!==0){
+                let removeElement = msgImp.firstElementChild;
+                console.log(removeElement);
+                removeElement.remove();
+            }
+
             let currentButton = e.currentTarget;
             let currentInput = currentButton.previousElementSibling.value;
             console.log(currentInput);
@@ -20,8 +29,8 @@ let addState = document.getElementById('addBtn');
             let displayInput = document.createElement('li');
             displayInput.classList.add('list-group-item');
             displayInput.className="list-group-item d-flex justify-content-center";
-            displayInput.innerHTML = `<p class="flex-grow-1">${currentInput}</p> 
-                <button id="yellowBtn" class="btn btn-warning">Edit</button> 
+            displayInput.innerHTML = `<h5 class="flex-grow-1">${currentInput}</h5> 
+                <button id="yellowBtn" class="btn btn-warning" onclick="editBook(this)">Edit</button> 
                 <button id="redBtn" class="btn btn-danger mx-1" onclick="removeBook(this)">Remove</button>`
 
             let parentBlock = document.getElementById('parentList');
@@ -29,16 +38,38 @@ let addState = document.getElementById('addBtn');
         }
 
         function removeBook(currentEle){
+            console.log(currentEle.parentElement)
+            currentEle.parentElement.remove();
+            
             if(parentList.childElementCount===0){
                 let msgIntend = document.getElementById('msgImp');
-                const h3 = document.createElement("h3");
+                const h3 = document.createElement("h5");
                 const textNode = document.createTextNode("Please insert your desire books !");
                 h3.appendChild(textNode);
                 msgIntend.appendChild(h3);
     
             }
+        }
+
+        function editBook(currentElement){
+            if(currentElement.textContent=="Done"){
+                currentElement.textContent = "Edit";
+                let prevInput = currentElement.previousElementSibling.value;
+                let editedElement = document.createElement('h4');
+                editedElement.className = "flex-grow-1";
+                editedElement.textContent = prevInput;
+                currentElement.parentElement.replaceChild(editedElement,currentElement.previousElementSibling);
+
+            }
             else{
-            console.log(currentEle.parentElement)
-            currentEle.parentElement.remove();
+                let prevInput = currentElement.previousElementSibling.textContent;
+                currentElement.textContent = "Done"
+                let newInput = document.createElement('input');
+                newInput.type = "text";
+                newInput.placeholder = "Book Name";
+                newInput.className = "form-control";
+                newInput.value = prevInput;
+                currentElement.parentElement.replaceChild(newInput,currentElement.previousElementSibling);
             }
         }
+
